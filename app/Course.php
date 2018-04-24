8310a306a4f9688f338d06b0a610d6b2d9b0affa
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-04-24 17:50:09 +0800

    Add soft deleting trait

--- app/Course.php
+++ app/Course.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
 {
+    use SoftDeletes;
+    
     protected $guarded = [];
 }
