5a27f0c4c9c6be9aaa43c21ff7d73108f309a8cf
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-04-25 14:53:24 +0800

    Update seeder to include newly added column price

--- database/seeds/BranchCoursesTableSeeder.php
+++ database/seeds/BranchCoursesTableSeeder.php
@@ -15,60 +15,70 @@ class BranchCoursesTableSeeder extends Seeder
         BranchCourse::create([
             'branch_id' => 1,
             'course_id' => 1,
+            'price' => 10000.00,
             'added_by' => 1
         ]);
 
         BranchCourse::create([
             'branch_id' => 1,
             'course_id' => 2,
+            'price' => 10000.00,
             'added_by' => 1
         ]);
 
         BranchCourse::create([
             'branch_id' => 2,
             'course_id' => 1,
+            'price' => 10000.00,
             'added_by' => 1
         ]);
 
         BranchCourse::create([
             'branch_id' => 2,
             'course_id' => 2,
+            'price' => 10000.00,
             'added_by' => 1
         ]);
 
         BranchCourse::create([
             'branch_id' => 3,
             'course_id' => 1,
+            'price' => 10000.00,
             'added_by' => 1
         ]);
 
         BranchCourse::create([
             'branch_id' => 3,
             'course_id' => 2,
+            'price' => 10000.00,
             'added_by' => 1
         ]);
 
         BranchCourse::create([
             'branch_id' => 4,
             'course_id' => 1,
+            'price' => 10000.00,
             'added_by' => 1
         ]);
 
         BranchCourse::create([
             'branch_id' => 4,
             'course_id' => 2,
+            'price' => 10000.00,
             'added_by' => 1
         ]);
 
         BranchCourse::create([
             'branch_id' => 5,
             'course_id' => 1,
+            'price' => 10000.00,
             'added_by' => 1
         ]);
 
         BranchCourse::create([
             'branch_id' => 5,
             'course_id' => 2,
+            'price' => 10000.00,
             'added_by' => 1
         ]);
     }
