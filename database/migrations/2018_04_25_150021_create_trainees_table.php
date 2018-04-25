d70252375e0b759fd70641a59d3a78f8ae8b9f47
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-04-25 17:58:29 +0800

    Add trainee model, migration and seeder

---
+++ database/migrations/2018_04_25_150021_create_trainees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTraineesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainees', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->string('mobile_number');
+            $table->string('rank');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainees');
+    }
+}
