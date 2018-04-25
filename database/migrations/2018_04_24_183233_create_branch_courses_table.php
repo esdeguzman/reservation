1bb335ead22e2e7313697eb80d0e328a048c0be5
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-04-25 09:16:13 +0800

    Add branch course model and migration
    Added branch course and migration, model relationship to course and branch already included

---
+++ database/migrations/2018_04_24_183233_create_branch_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBranchCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branch_courses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('branch_id');
+            $table->unsignedInteger('course_id');
+            $table->unsignedInteger('added_by');
+            $table->unsignedInteger('deleted_by')->nullable();
+            $table->unique(['course_id', 'branch_id']);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branch_courses');
+    }
+}
