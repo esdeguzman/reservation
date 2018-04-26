42cc7ad8a8b3a0a52c49ac8a550b7588ae01fc44
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-04-26 18:44:53 +0800

    Trial on trainee registration

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -10,25 +10,25 @@ use Illuminate\Support\Facades\Auth;
 class ReservationsController extends Controller
 {
     public function store(Request $request) {
-        return $request->all();
-//        $user = User::create([
-//            'email' => $request->email,
-//            'username' => $request->username,
-//            'password' => bcrypt($request->password),
-//        ]);
-//
-//        Trainee::create([
-//            'user_id' => $user->id,
-//            'first_name' => $request->first_name,
-//            'middle_name' => $request->middle_name,
-//            'last_name' => $request->last_name,
-//            'mobile_number' => $request->mobile_number,
-//            'rank' => $request->rank,
-//        ]);
-//
-//        Auth::loginUsingId($user->id);
-//
-//        return User::with('trainee')->where('id', $user->id)->get();
+//        return $request->all();
+        $user = User::create([
+            'email' => $request->email,
+            'username' => $request->username,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Trainee::create([
+            'user_id' => $user->id,
+            'first_name' => $request->first_name,
+            'middle_name' => $request->middle_name,
+            'last_name' => $request->last_name,
+            'mobile_number' => $request->mobile_number,
+            'rank' => $request->rank,
+        ]);
+
+        Auth::loginUsingId($user->id);
+
+        return User::with('trainee')->where('id', $user->id)->get();
 
     }
 }
