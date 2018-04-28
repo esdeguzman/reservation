ef752153a2265d0d6b1b3b4e468fc5d14d55d98e
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-04-28 13:38:08 +0800

    Add reservation api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,24 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/reserve/{traineeId}/{scheduleId}', function($traineeId, $scheduleId) {
+   $schedule = \App\Schedule::with('branchCourse')->where('id', $scheduleId)->first();
+   $user = \App\User::with('trainees')->where('id', $traineeId)->first();
+
+    $reservation = \App\Reservation::create([
+       'branch_id' => $schedule->branchCourse->branch_id,
+       'trainee_id' => $user->trainees->first()->id,
+       'schedule_id' => $scheduleId,
+   ]);
+
+    $reservationCode = \App\Branch::find($schedule->branchCourse->branch_id)->code . '0' . $traineeId . '-' . '052018-0'
+        . count(\App\Reservation::where('branch_id', $schedule->branchCourse->branch_id)->get());
+
+    $reservation->code = $reservationCode;
+    $reservation->save();
+
+    $user->notify(new \App\Notifications\ReservationSuccessful($reservation));
+
+    return 'Schedule successfully reserved!';
+});
