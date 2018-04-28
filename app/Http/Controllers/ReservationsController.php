c84eb14222919c61a138a13e55bec57141bd9673
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-04-28 13:37:41 +0800

    Place trainee creation to correct controller

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -9,26 +9,5 @@ use Illuminate\Support\Facades\Auth;
 
 class ReservationsController extends Controller
 {
-    public function store(Request $request) {
-//        return $request->all();
-        $user = User::create([
-            'email' => $request->email,
-            'username' => $request->username,
-            'password' => bcrypt($request->password),
-        ]);
 
-        Trainee::create([
-            'user_id' => $user->id,
-            'first_name' => $request->first_name,
-            'middle_name' => $request->middle_name,
-            'last_name' => $request->last_name,
-            'mobile_number' => $request->mobile_number,
-            'rank' => $request->rank,
-        ]);
-
-        Auth::loginUsingId($user->id);
-
-        return User::with('trainees')->where('id', $user->id)->first();
-
-    }
 }
--- app/Http/Controllers/TraineesController.php
+++ app/Http/Controllers/TraineesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Trainee;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -19,5 +20,28 @@ class TraineesController extends Controller
         Auth::logout($user);
 
         return 'User has been logged out!';
+    }
+
+    public function store(Request $request) {
+//        return $request->all();
+        $user = User::create([
+            'email' => $request->email,
+            'username' => $request->username,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Trainee::create([
+            'user_id' => $user->id,
+            'first_name' => $request->first_name,
+            'middle_name' => $request->middle_name,
+            'last_name' => $request->last_name,
+            'mobile_number' => $request->mobile_number,
+            'rank' => $request->rank,
+        ]);
+
+        Auth::loginUsingId($user->id);
+
+        return User::with('trainees')->where('id', $user->id)->first();
+
     }
 }
